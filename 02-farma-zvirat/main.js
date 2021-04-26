afdbac5604f85bca6bb9ad8fbbbc01ed06689491
Jana Bernkopf <>
2021-04-26 20:07:10 +0200

    přidala se ovce

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -15,6 +15,8 @@ let ovecka = {
     let zvire = document.createElement("div");
     zvire.className = "zvire";
 
+    let ovce = document.createElement("div");
+    ovce.className = "zvire";
 
 // 2. Nyni vytvor dva elementy pro obrazek a jmeno zvirete, ktere budou uvnitr naseho obalujiciho div elementu zvire.
     //2.1. Vytvor img element s tridou 'foto'. Jako src obrazku pridej hodnotu objektu krava.
@@ -22,6 +24,11 @@ let ovecka = {
     let fotoZvirete = document.createElement("img");
     fotoZvirete.className = "foto";
     fotoZvirete.src = krava.foto;
+
+    let fotoOvce = document.createElement("img");
+    fotoOvce.className = "foto";
+    fotoOvce.src = ovecka.foto;
+    
     
 
     //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
@@ -30,10 +37,17 @@ let ovecka = {
     jmenoZvirete.className = "jmeno";
     jmenoZvirete.innerHTML = krava.jmeno;
 
+    let jmenoOvce = document.createElement("span");
+    jmenoOvce.className = "jmeno";
+    jmenoOvce.innerHTML = ovecka.jmeno;
+
     //2.3. Zabal fotoZvire a jmenoZvirete do obalovaciho divu zvire, (psst zvire.appendChild)
 
     zvire.appendChild(fotoZvirete);
     zvire.appendChild(jmenoZvirete);
+
+    ovce.appendChild(fotoOvce);
+    ovce.appendChild(jmenoOvce);
 
 
 // 3. Nakonec pojdme nas kod zapojit do html stranky. Na html strance vyber existujici element s tridou farma.
@@ -41,6 +55,7 @@ let ovecka = {
 
   let farma = document.querySelector(".farma");
   farma.appendChild(zvire);
+  farma.appendChild(ovce);
 
 
 // Bonus
